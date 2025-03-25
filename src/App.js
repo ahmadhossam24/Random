488b1185fce19020ec186cc26b26f6f1896c143e
ahmadhossam24 <70440432+ahmadhossam24@users.noreply.github.com>
2025-03-26 00:19:12 +0200

    firstPush

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import './App.css';
-import SpinPage from "./pages/spin_page";
-import { SpinProvider } from "./context/spin_context";
-import PrepareTablesPage from './pages/prepare_tables_page';
+import './App.scss';
+import UserTypesCarouselPage from "./pages/user_types_carousel_page";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <SpinProvider>
-      <Router>
-        <Routes>
-          <Route path='/' element={<SpinPage />} />
-          <Route path='/prepare-table' element={<PrepareTablesPage />} />
-        </Routes>
-      </Router>
-    </SpinProvider>
+    <Router>
+      <Routes>
+        <Route path='/' element={<UserTypesCarouselPage />} />
+      </Routes>
+    </Router>
   )
   ;
 }
